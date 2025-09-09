3988ad2c69f208809e2c574e3d4ee7b0b17078f8
Gabriel <greyes15@mail.stmarytx.edu>
2025-09-09 09:29:25 -0500

    Add Database

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +90,20 @@ WSGI_APPLICATION = "config.wsgi.application"
 #}
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# This allows you to use SQLite locally and PostgreSQL on Render
+if 'DATABASE_URL' in os.environ:
+    # Production (Render)
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('postgresql://crmai_db_user:JtOBRBBlAaWbZ8TN738Jgs1cc7AR2jxH@dpg-d303d6mr433s73et4lb0-a/crmai_db'))
     }
-}
+else:
+    # Local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
